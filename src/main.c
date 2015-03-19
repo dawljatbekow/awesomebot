655766d5ea1d587af74fe625929dd07c68944321
dawljatbekow <s.dawljatbekow@gmail.com>
2015-03-19 15:20:48 +0100

    Roboterfunktionen soweit vollständig 19.03.15

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 int knoten;									//Zaehlen der Knoten
 int kreuzung[3];							//Array mit {Links, Rechts, Geradeaus} kodierung
 int gegenstand;								//Gegenstandszaehlung
+int lichtgrenze;
 
 /* nxtOSEK hook to be invoked from an ISR in category 2 */
 void user_1ms_isr_type2(void) {
@@ -19,7 +20,7 @@ void stop(){
 	/*
 	 * Funktion zum stoppen der Motoren
 	 */
-	nxt_motor_set_speed(NXT_PORT_A, 0, 1);
+	nxt_motor_set_speed(NXT_PORT_C, 0, 1);
 	nxt_motor_set_speed(NXT_PORT_B, 0, 1);
 }
 
@@ -55,21 +56,29 @@ void richtiglenken(int s, int v){
 	int i=0;
 	if (s>0){									//Fallunterscheidung bez. der Lenkrichtung
 		while (i<=s){							//Auslenkung beider Raeder durch gleichmaessige
-				lenken(NXT_PORT_A, 2, v);		//Beschleunigung beider Motoren
-				lenken(NXT_PORT_B, -2, v+4);	//Korrektur am rechten Motor noetig
+				lenken(NXT_PORT_C, 2, v);		//Beschleunigung beider Motoren
+				lenken(NXT_PORT_B, -3, v+5);	//Korrektur am rechten Motor noetig
 				i=i+2;
 		}
 	}
 	else{
 		while (i>=s){
-				lenken(NXT_PORT_A, -2, v);		//wie oben, nur andere Richtung
-				lenken(NXT_PORT_B, 2, v+4);
+				lenken(NXT_PORT_C, -2, v);		//wie oben, nur andere Richtung
+				lenken(NXT_PORT_B, 3, v+5);
 				i=i-2;
 		}
 	}
 	stop();										//Ende des Lenkvorgangs
 }
 
+void kalibrierung(){
+	ecrobot_set_light_sensor_active(NXT_PORT_S4);		//Lichtsensor initiallisieren
+	int a=ecrobot_get_light_sensor(NXT_PORT_S4);
+	richtiglenken(20,30);
+	int b=ecrobot_get_light_sensor(NXT_PORT_S4);
+	richtiglenken(-20,30);
+	lichtgrenze=((a+2*b)/3);
+}
 
 void orientierung(int lenken_v){
 	/*
@@ -78,7 +87,7 @@ void orientierung(int lenken_v){
 	 * uebergeben wird die Lenkgeschwindigkeit (leneken_v)
 	 */
 	int b=0;
-	while(b<20){							//Gesucht wird um 20LE nach recht und 40LE nach links
+	while(b<18){							//Gesucht wird um 20LE nach recht und 40LE nach links
 		richtiglenken(2, lenken_v);			//Um zur Ursprungsfunktion zu gelangen, wird um 20LE
 		b=b+2;								//wieder nach rechts gedreht
 	}
@@ -88,8 +97,8 @@ void fahren(int v){
 	/*
 	 *Funktion zum Vorfahren zur ungefairen Mitte des Knotens
 	 */
-	nxt_motor_set_speed(NXT_PORT_A, v, 1);		//Motor am Port_B faehrt langsamer als der am Port_A
-	nxt_motor_set_speed(NXT_PORT_B, v+2, 1);	//dies wird hier mit v+3 korrigiert
+	nxt_motor_set_speed(NXT_PORT_C, v, 1);		//Motor am Port_B faehrt langsamer als der am Port_A
+	nxt_motor_set_speed(NXT_PORT_B, v, 1);	//dies wird hier mit v+3 korrigiert
 	systick_wait_ms(1000);						//nach 1 sekunde fahren ist man ungefair in der Mitte des Knoten
 	stop();
 }
@@ -103,14 +112,14 @@ void suchen(int lenken_v){
 
 	ecrobot_set_light_sensor_active(NXT_PORT_S4);	//Lichtsensor initiallisieren
 
-	int x=250, s=0, a=ecrobot_get_light_sensor(NXT_PORT_S4);//x ist die Strecke der abgetasteten Drehung
+	int x=190, s=0, a=ecrobot_get_light_sensor(NXT_PORT_S4);//x ist die Strecke der abgetasteten Drehung
 															//250 = eine Umdrehung
 	while(s<x){
 		richtiglenken(2, lenken_v);				//gedreht wird in 2-er Schritten
 		s=s+2;									//zurueckgelegte Schritte werden in s gespeichert
 
 		a=ecrobot_get_light_sensor(NXT_PORT_S4);
-		if(a>500){
+		if(a>lichtgrenze){
 			if((s >= 0 && s<= x/16) || (s >= x-x/16)){	//Schwarte Linie am Anfang der Drehug oder am Ende
 				kreuzung[2]=1;							//entspicht Weg geradeaus vorhanden
 														//wird an der letzten Position im Array gespeichert
@@ -132,47 +141,47 @@ void folgen(int v, int lenken_v){
 	 * erkennen eines Gegenstandes
 	 * uebergeben werden die Fahrgeschwindigkeit (v) und die Lenkgeschwindigkeit (lenk_v)
 	 */
-	ecrobot_set_light_sensor_active(NXT_PORT_S4);		//Lichtsensor initiallisieren
-	int a= ecrobot_get_light_sensor(NXT_PORT_S4);		//Lichtwert auslesen
 
-	U8 touch=ecrobot_get_touch_sensor(NXT_PORT_S1);
+	int a= ecrobot_get_light_sensor(NXT_PORT_S4);		//Lichtwert auslesen
+	U8 touch1=0, touch2=0;
 
 	while(knoten==0){									//Solange man nicht am Knoten ist, wiederholen der Wegfolgenroutine
 
-		while(a>500 && touch==0){						//fahren mit gesetzter Geschwindigkeit
-			nxt_motor_set_speed(NXT_PORT_A, v, 1);		//bei gleichzeitigem Abtasten der Linie
-			nxt_motor_set_speed(NXT_PORT_B, v+2, 1);
+		while(a>lichtgrenze && (touch1==0 && touch2==0)){						//fahren mit gesetzter Geschwindigkeit
+			touch1=ecrobot_get_touch_sensor(NXT_PORT_S1);//faehrt solange kein Gegenstand im Weg ist
+			touch2=ecrobot_get_touch_sensor(NXT_PORT_S2);
+			nxt_motor_set_speed(NXT_PORT_C, v, 1);		//bei gleichzeitigem Abtasten der Linie
+			nxt_motor_set_speed(NXT_PORT_B, v, 1);
 			a = ecrobot_get_light_sensor(NXT_PORT_S4);
-			touch=ecrobot_get_touch_sensor(NXT_PORT_S1);//faehrt solange kein Gegenstand im Weg ist
+
 		}
 		stop();											//bei Verlust der Linie oder Gegenstand stoppen
 
-		if(touch==1){									//Routine nach Erkennung eines Gegenstandes
+		if(touch1>0 || touch2>0){									//Routine nach Erkennung eines Gegenstandes
 			ecrobot_sound_tone(220, 1000, 20);			//Ausgabe eines vorgeschriebenen Tons
 			systick_wait_ms(10000);						//warte 10s auf entnahme des Gegenstandes
 			gegenstand++;								//Speicherung des Gegenstandes
-			touch =0;
+			touch1 = touch2 = 0;
 		}
 
 		int b=0;										//Linie wird gesucht durch Auslenkung um 20 LE
-		while(b<18 && a<500){							//nach rechts
+		while(b<18 && a<=lichtgrenze){							//nach rechts
 			richtiglenken(2, lenken_v);
 			a = ecrobot_get_light_sensor(NXT_PORT_S4);
 			b=b+2;
 			}
 		b=0;
-		while(b<46 && a<500){							//dann um 40LE nach links
-			richtiglenken(-2, lenken_v+5);
+		while(b<42 && a<=lichtgrenze){							//dann um 40LE nach links
+			richtiglenken(-2, lenken_v);
 			a = ecrobot_get_light_sensor(NXT_PORT_S4);
 			b=b+2;
 		}
-		if (a<500){										//Wenn Linie nicht gefunden wird, dann muss es ein Knoten sein
+		if (a<lichtgrenze){										//Wenn Linie nicht gefunden wird, dann muss es ein Knoten sein
 			knoten=1;
 		}
 	}
-	orientierung(lenken_v);				//bei erreichtem Knoten wird der Roboter gerade gestellt,
-	fahren(30);							//ungefair zur Mitte des Knotens bewegt
-	suchen(30);							//und die Suche nach Wegen am Knoten wird eingeleitet
+	orientierung(lenken_v);				//bei erreichtem Knoten wird der Roboter gerade gestellt und
+	fahren(35);							//ungefair zur Mitte des Knotens bewegt
 }
 
 void fahre_richtung(int r){
@@ -181,10 +190,10 @@ void fahre_richtung(int r){
 	 * uebergeben wird die Richtung (r)
 	 */
 	switch(r){									//Entscheidung zwischen den moeglichen Richtungen
-	case 0: richtiglenken(-250/2, 30);	break;  //0=Drehung nach links
-	case 1: richtiglenken( 250/2, 30);	break;	//1=Drehung nach rechts
+	case 0: richtiglenken(-200/2, 30);	break;  //0=Drehung nach links
+	case 1: richtiglenken( 200/2, 30);	break;	//1=Drehung nach rechts
 	case 2: break;								//2=keine Drehung, da Ausgangslage nach vorne zeigt
-	case 3: richtiglenken( 250, 30);	break;	//3=Drehung nach hinten
+	case 3: richtiglenken( 200, 30);	break;	//3=Drehung nach hinten
 	}
 
 }
@@ -215,12 +224,15 @@ TASK(OSEK_Main_Task) {
 	/*
 	 * Hauptfunktion des Roboters
 	 */
+	lichtgrenze=0;
+	systick_wait_ms(2000);
+	kalibrierung();
 	int i=0;
 	gegenstand=0;
 	while (1) {
-
 		knoten=0;
 		folgen(50, 20);
+		suchen(30);
 		anzeige(kreuzung);
 		fahre_richtung(i);
 		i++;
